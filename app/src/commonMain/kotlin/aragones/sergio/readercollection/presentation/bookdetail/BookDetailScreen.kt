eddc81efcd504d5646839eb5dc43b1dc1bf035c5
Sergio Aragonés <sergioat7@gmail.com>
2026-09-14 17:49:20 +0200

    Add status bar padding to BookDetailScreen

--- app/src/commonMain/kotlin/aragones/sergio/readercollection/presentation/bookdetail/BookDetailScreen.kt
+++ app/src/commonMain/kotlin/aragones/sergio/readercollection/presentation/bookdetail/BookDetailScreen.kt
@@ -14,10 +14,13 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -135,6 +138,7 @@ fun BookDetailScreen(
 ) {
     Column(
         modifier = modifier
+            .windowInsetsPadding(WindowInsets.statusBars)
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.primary),
         verticalArrangement = Arrangement.Center,
